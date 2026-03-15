36b0ed555a5603420e5f10073e54fdb1ef95a6cf
albar08 <58755102+albar08@users.noreply.github.com>
2026-03-15 16:25:22 +0800

    Update script.js
    Removing changes

--- script.js
+++ script.js
@@ -97,17 +97,3 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.1 });
 
 document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
-
-let mybutton = document.getElementById("backToTop");
-
-window.onscroll = function() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-};
-
-function topFunction() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-}
